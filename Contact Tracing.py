eca14e6f268b1fdf0025384e7ef77b7ff80e9e5a
[DVenus19] <[donasco.venus.12232002@gmail.com]>
2022-11-13 00:36:35 +0800

    commit (testing 3)

--- Contact Tracing.py
+++ Contact Tracing.py
@@ -1,4 +1,4 @@
 print("~~~~~~~~~~Dictionary - Contact Tracing~~~~~~~~~~")
-print("~~~~~~~~~~Programmed by : Donasco,Venus M.~~~~~~~~~~")
-print("~~~~~~~~~~BSCOE 2-2 ~~~~~~~~~~")
+print("~~~~~~~~~~Programmed by: Donasco,Venus M.~~~~~~~~~~")
+print("~~~~~~~~~~BSCOE 2-2~~~~~~~~~~")
 #Python Program for Contact Tracing using Dictionary to store info
