73a48d582722d1a453dd443416503c318a1e44d3
[DVenus19] <[donasco.venus.12232002@gmail.com]>
2022-11-13 00:41:48 +0800

    commit1

--- Contact Tracing.py
+++ Contact Tracing.py
@@ -2,3 +2,6 @@ print("~~~~~~~~~~Dictionary - Contact Tracing~~~~~~~~~~")
 print("~~~~~~~~~~Programmed by: Donasco,Venus M.~~~~~~~~~~")
 print("~~~~~~~~~~BSCOE 2-2~~~~~~~~~~")
 #Python Program for Contact Tracing using Dictionary to store info
+alldict = {}
+while True:
+    print('Menu: 1. Add an item , 2. Search , 3. Exit(y/n)')
